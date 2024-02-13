fc3ce7b8ae76a26ab8213f27fcd803a962fe0553
aaron-rub <aaronr06138@gmail.com>
2024-02-13 11:53:23 -0800

    NPM+Backend debug

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Income/IncomeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.Income;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IncomeRepository extends CrudRepository<Income, Long> {
-}
